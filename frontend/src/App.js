0e0b7957ebca9127a2dbe760d9423c2b0317e607
alireza <es.alrza@gmail.com>
2024-11-03 21:35:22 +0330

    [Feature] Implement Link and Roter for all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.js";
+import ProductScreen from "./screens/ProductScreen.js";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductScreen = () => {
+  return (
+    <div>
+      <>This is a Big Product</>
+    </div>
+  );
+};
+
+export default ProductScreen;
