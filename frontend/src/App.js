13161baab6cde7f39b18f3466f6ad526060f5db9
alireza <es.alrza@gmail.com>
2024-11-03 20:26:48 +0330

    [Feature] HomeScreen can show the Products [Feature] Rating added to each product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen.js";
 const App = () => {
   return (
     <>
-      <h1>Welcome to my first project!</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 };
---
+++ frontend/src/Products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/image/airpods.jpg",
+    description:
+      "Bluetooth is just Blah blah blah but this" +
+      " is a real awsume boo boo bi bi but bum bum." +
+      " it has a real blue title bool bool.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 83.99,
+    countInStock: 3,
+    rating: 4.3,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Iphone 11 pro max 256 memory",
+    image: "/image/iphone11.jpg",
+    description:
+      "A verry verry verry good phone for the " +
+      " lazy users that just want a blue glass" +
+      " for their cars now the can go faster.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 899.99,
+    countInStock: 4,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "3",
+    name: "PS5 2TB ",
+    image: "/image/ps5.jpg",
+    description:
+      "Its a Tuf one with a good history" +
+      " for the most years of our work and" +
+      " this is the last one of you mom.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 749.88,
+    countInStock: 2,
+    rating: 4.9,
+    numReviews: 16,
+  },
+  {
+    _id: "4",
+    name: "Asus TUF gaming RTX5060 ",
+    image: "/image/asusLaptop.jpg",
+    description:
+      "An aluminom back case with a mirror" +
+      " on left is the real laptop that you wish" +
+      " it can code for free for you.",
+    brand: "Asus",
+    category: "Electronics",
+    price: 430,
+    countInStock: 7,
+    rating: 4.2,
+    numReviews: 26,
+  },
+  {
+    _id: "5",
+    name: "Nokia 111",
+    image: "/image/nokia.jpg",
+    description: "You going to have 263 hours call time with 1200ma battery",
+    brand: "Nokia",
+    category: "Electronics",
+    price: 16,
+    countInStock: 4,
+    rating: 4.9,
+    numReviews: 3,
+  },
+];
+export default products;
