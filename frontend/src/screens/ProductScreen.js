9ef3df8befbc95d30784ac5b3e0ab20b21e16465
alireza <es.alrza@gmail.com>
2024-11-06 13:20:21 +0330

    [Feature] ProductScreen added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,10 +1,74 @@
 import React from "react";
-
+import { Link, useParams } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../Products";
 const ProductScreen = () => {
+  const { id } = useParams();
+  const product = products.find((p) => p._id === id);
   return (
-    <div>
-      <>This is a Big Product</>
-    </div>
+    <>
+      <Link className="btn btn-light my-3" to={"/"}>
+        Go Back
+      </Link>
+      <Row className="mx-4">
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={6}>
+          <Row>
+            <h3>{product.name}</h3>
+          </Row>
+          <Row>
+            <Col>
+              <ListGroup c variant="flush">
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                <ListGroup.Item>
+                  Description: {product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col>
+              <Card className="mt-auto mb-5">
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col className="text-end">
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col className="text-end">
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-dark w-100 rounded-0"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
   );
 };
 
